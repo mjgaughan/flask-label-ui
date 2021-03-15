b0593b6d837f73e43068a66c8a9df4ed6239030d
John Foley <johnf@middlebury.edu>
2021-03-15 14:05:04 -0400

    starter labeling code

---
+++ dataset.py
@@ -0,0 +1,70 @@
+
+import csv
+import json
+from dataclasses import dataclass, field
+from typing import Dict, Any, List
+import random
+
+## TODO: configure this for yourself.
+IDENTIFIER_FIELD = "id"
+
+## What buttons should we show the user? Note: ``Other``` appears automatically.
+DEFAULT_BUTTONS = ["Nice", "Ugly", "Bad Name"]
+
+@dataclass
+class Example:
+    """This class holds a single example, in all it's weirdness."""
+
+    """Your example MUST have a UNIQUE identifier. If your identifiers are numeric, just make them strings anyway."""
+    id: str
+    """This is the file path that was processed to read this example."""
+    source_file: str
+    """This is the row number from the source data file that generated the example."""
+    source_line: int
+    """Miscellaneous features; default: empty-dict"""
+    features: Dict[str, Any] = field(default_factory=dict)
+
+    def json_str(self) -> str:
+        """ Not a very user-friendly way to view the data, but it's possible! """
+        return json.dumps(self.features, indent=2, sort_keys=True)
+
+
+def convert(source_file: str, source_line: int, data: Dict[str, Any]) -> Example:
+    # TODO: MAYBE modify this part; which is the key?
+    keep = Example(
+        id=data[IDENTIFIER_FIELD],
+        source_file=source_file,
+        source_line=source_line,
+        features=data,
+    )
+    return keep
+
+
+def load_csv_examples(path: str) -> Dict[str, Example]:
+    available = {}
+    with open(path) as fp:
+        rows = csv.reader(fp)
+        header = next(rows)
+        for i, row in enumerate(rows):
+            keep = convert(path, i, dict(zip(header, row)))
+            available[keep.id] = keep
+    return available
+
+
+def load_jsonl_examples(path: str) -> Dict[str, Example]:
+    available = {}
+    with open(path) as fp:
+        for i, line in enumerate(fp):
+            keep = convert(path, i, json.loads(line))
+            available[keep.id] = keep
+    return available
+
+
+def load_examples() -> Dict[str, Example]:
+    return load_csv_examples('html_colors.csv')
+
+if __name__ == '__main__':
+    examples = load_examples()
+    ids = list(examples.keys())
+    random_id = random.choice(ids)
+    print(examples[random_id])
---
+++ server.py
@@ -0,0 +1,124 @@
+from flask import Flask, session, redirect, url_for, request, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Connection
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+import os, random
+from datetime import datetime
+from dataclasses import dataclass
+
+# see dataset.py
+import dataset
+
+
+DATASET = dataset.load_examples()
+app = Flask(__name__)
+if app.debug:
+    app.config.update({
+        'SECRET_KEY': 'Just in Debug Mode!'
+    })
+else:
+    app.config.update({
+        'SECRET_KEY': os.urandom(24)
+    })
+engine = create_engine('sqlite:///labels.db')
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Label(Base):
+    __tablename__ = 'labels'
+    id = Column(Integer, primary_key=True)
+    when = Column(DateTime)
+    what = Column(String)
+    who = Column(String)
+    label = Column(String)
+
+Base.metadata.create_all(engine)
+
+def get_user():
+    if 'user' not in session:
+        return None
+    return session['user']
+
+def redirect_login():
+    return redirect(url_for('login', destination=request.full_path))
+
+@app.route('/')
+def front_page():
+    user = get_user()
+    if user is None:
+        return redirect_login()
+    next_id = random.choice(list(DATASET.keys()))
+    return redirect(url_for('label', id=next_id))
+
+def process_label(label: str) -> str:
+    return label.upper().replace(' ', '_').replace('-', '_')
+
+@app.route("/label/<id>")
+def label(id: str):
+    user = get_user()
+    if user is None:
+        return redirect_login()
+    example = DATASET[id]
+    labels = []
+    db = Session()
+    for (lbl,) in db.query(Label.label).filter(Label.who==user, Label.what==id):
+        labels.append(lbl)
+    print(labels)
+    db.close()
+    add_buttons = []
+    delete_buttons = []
+    for button in dataset.DEFAULT_BUTTONS:
+        as_lbl = process_label(button)
+        if as_lbl in labels:
+            delete_buttons.append(as_lbl)
+            labels.remove(as_lbl)
+        else:
+            add_buttons.append(as_lbl)
+    for lbl in labels:
+        delete_buttons.append(lbl)
+    return render_template('label_one.j2', example=example, add_buttons=add_buttons, delete_buttons=delete_buttons)
+
+@app.route("/form/label", methods=["POST"])
+def post_label():
+    user = get_user()
+    if user is None:
+        return redirect_login()
+    id = request.form['id']
+    assert(id in DATASET)
+    db = Session()
+    label = Label(who=user, when=datetime.now(), what=id, label=process_label(request.form['label']))
+    db.add(label)
+    db.commit()
+    db.close()
+    return redirect(url_for('label', id=id))
+
+@app.route("/form/undo_label", methods=["POST"])
+def undo_label():
+    user = get_user()
+    if user is None:
+        return redirect_login()
+    id = request.form['id']
+    assert(id in DATASET)
+    db = Session()
+    for (row_id,) in db.query(Label.id).filter(Label.who==user, Label.what==id, Label.label==process_label(request.form['label'])):
+        row = db.query(Label).get(row_id)
+        print("DELETE: ", row)
+        db.delete(row)
+    db.commit()
+    db.close()
+    return redirect(url_for('label', id=id))
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == 'GET':
+        return render_template('login.j2', destination=request.args.get('destination', '/'))
+    else:
+        session['user'] = request.form['user']
+        return redirect(request.form.get('destination', '/'))
+
+@app.route('/logout')
+def logout():
+    del session['user']
+    return redirect('/login')
